d02bb38178dcea3b468495d4b1fd153a51476b8c
krnveersharma <karansharma10293847@gmail.com>
2025-07-17 22:25:06 +0530

    added error reason

--- server/dbConnection/setupDbConnection.go
+++ server/dbConnection/setupDbConnection.go
@@ -3,6 +3,7 @@ package dbconnection
 import (
 	"database/sql"
 	"errors"
+	"fmt"
 
 	"github.com/krnveersharma/Statuses/config"
 	_ "github.com/lib/pq"
@@ -16,7 +17,7 @@ func SetupDbConnection(config config.Config) (*sql.DB, error) {
 
 	err = db.Ping()
 	if err != nil {
-		return nil, errors.New("Unable to connect to the database %v DSN is: %v",err.Error()+, config.Dsn)
+		return nil, fmt.Errorf("Unable to connect to the database due to error: %s DSN is: %s", err.Error(), config.Dsn)
 	}
 
 	return db, nil
